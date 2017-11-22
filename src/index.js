dee66b30053befe27e11ce655ff5085ff5bd9626
jelf <353742991@qq.com>
2017-11-22 23:55:20 +0800

    feat: 引入阿里移动端解决方案flexible

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/js/flexible.js'
+import './assets/styles/flexible.less'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
