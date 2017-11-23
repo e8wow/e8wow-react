534689e0bae4cca8f4efe11b22712ba25da4f349
jelf <353742991@qq.com>
2017-11-23 12:34:22 +0800

    refactor: 暴露并修改package.json，增加EsLint给WebStorm

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    extends: 'react-app',
+    rules: {
+        'arrow-parens': 0,
+        'indent': [2, 4],// 缩进
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/js/flexible.js'
-import './assets/styles/flexible.less'
+import './assets/styles/flexible.less';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
 import FastClick from 'fastclick';
+
 if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         FastClick.attach(document.body);
     }, false);
 }
